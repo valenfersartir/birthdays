151b76e0b952e89a6d47c58117b7d0e020b1df1d
valenfersartir <nelav2187@gmail.com>
2023-10-18 08:50:15 +0700

    tugasbirthdays

--- app.py
+++ app.py
@@ -1,59 +1,51 @@
-# import os untuk mengakses sistem database
-import os
-# import SQL untuk menggunakan bahasa SQL dalam python
-from cs50 import SQL
-# import tools utk website
-from flask import Flask, flash, jsonify, redirect, render_template, request, session
-# mengatur nama aplikasi
-app = Flask(__name__)
-# dipakai untuk koneksi ke database
-db = SQL("sqlite:///birthdays.db")
-# http://127.0.0.1:5000/
+import os #import os untuk mengakses sistem database
+
+from cs50 import SQL #import SQL untuk menggunakan bahasa SQL dalam phyton
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session #import tools untuk website
+
+app = Flask(__name__) #mengatur nama aplikasi
+
+db = SQL ("sqlite:///birthdays.db")
+
 @app.route("/", methods=["GET", "POST"])
+#ketika route "/" dipanggil/diakses, maka fungsi inde() dieksekusi
 def index():
-    # jika request yg dilakukan oleh pengguna adalah post, maka eksekusi kode dalam if
+    #jika requst yang dilakukan oleh pungguna adalah post, maka eksekusi kode dalam if 
     if request.method == "POST":
+        #access form data / membaca data pada yang diisikan pada form 
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day") 
 
-        # Access form data / membaca data yg diisikan pada form
-        name = request.form.get("name") # ambil data dari input name
-        month = request.form.get("month") # ambil data dari input month
-        day = request.form.get("day") # ambil data dari input day
-
-        # Insert data into database, masukan data name, month, dan day ke database 
+        #masukkan data ke database
         db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
 
-        # Go back to homepage, balik ke https://127.0.0.1:5000/
+        #balik ke https://127.0.0.1:5000/
         return redirect("/")
-
-#jika request selain POST, maka tampilkan data dari tabel birthdays
+        
+    # jika requestnya selain post, maka tampilkan data dari tabel birthdays  
     else:
-
         # ambil seluruh data dari tabel birthdays, simpan di variabel birthdays
         birthdays = db.execute("SELECT * FROM birthdays")
 
-        # salin isi variabel birthdays ke birthdays, lalu kirim ke undex.html
+        # salin isi variabel birtdays ke birhdays, lalu kirim ke index.html
         return render_template("index.html", birthdays=birthdays)
 
-@app.route("/")
-def valen():
-    return "halo saya valen"
-
-@app.route("/biografi")
-def biografi_valen():
-    return "sek smk"
-
-@app.route("/tentang")
-def tentang_valen():
-    return "tentang valen"
-
-@app.route("/contact")
-def contact_valen():
-    return "081273271862"
-
-@app.route("/hobi")
-def hobi_valen():
-    return "hobi valen turu"
-
-@app.route("/profesi")
-def profesional():
-    return render_template("gatau.html")
+@app.route("/edit/<id>", methods=["GET", "POST"])
+def edit_data(id):
+    if  request.method == "GET":
+        bday = db.execute("SELECT * FROM birthdays WHERE id = ?", id)[0]
+        print(bday)
+        return render_template("edit.html", bday=bday) 
+    elif request.method == "POST":
+        bday_name = request.form.get("name")   
+        bday_month = request.form.get("month")
+        bday_day = request.form.get("day")
+        db.execute('UPDATE birthdays set name = ?, month = ?, day = ? where id = ?' , bday_name, bday_month, bday_day, id)
+        return redirect("/")
+    
+@app.route("/delete/<id>", methods=["GET"])
+def delete_id(id):
+    db.execute("delete from birthdays where id = ?", id)
+    return redirect("/")
